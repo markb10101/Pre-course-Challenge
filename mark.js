69c17b05b56ab8770b1c6989c0b3562e732f77fb
markb10101 <mark.birchall@gmail.com>
2020-09-02 23:48:39 +0100

    Add files via upload
    rewrote html and css to remove superfluous imgs and code
    changed the way the site displays for mobile widths
    added js click to change random facts
    added js click to show relevant project info

--- mark.js
+++ mark.js
@@ -1,10 +1,13 @@
-
+///////////////////////////////////////
 // Add current time to page Footer
+
+// Store a pointer to the .clock element
 const clock = document.querySelector('.clock');
 
+// Write the date and time to the page
 const tickTock = () => {
 
-    // Store unformatted Date
+    // Get unformatted Date
     const currentTime = new Date();
 
     // Get Date
@@ -18,19 +21,33 @@ const tickTock = () => {
     // Build time string including leading zeroes
     var writeTime = `${date} - `;
     if (h<10) {writeTime += "0";}
-    writeTime += h + ":";
+        writeTime += h + ":";
     if (m<10) {writeTime += "0";}
-    writeTime += m + ":";
+        writeTime += m + ":";
     if (s<10) {writeTime += "0";}
-    writeTime += s;
+        writeTime += s;
 
     // Inject time string into .clock element
     clock.innerHTML = writeTime;
 
 }
 
+// Call tickTock on page load, so time appears immediately
+tickTock();
+
+// Then call tickTock every 1000ms, so time updates every second
+setInterval(tickTock, 1000)
+
+
+
+
+/////////////////////////////
 // Display a random MarkFax
+
+// Store a pointer to the 2nd paragraph of the .aside element in a variable
 var asidePar = document.querySelector('.aside > p:nth-child(2)');
+
+// Populate an array with some random facts
 var factsList = [
     "Mark spent a summer working as a Bingo Caller on Worthing's sea front.",
     "Mark once danced with Vic Reeves & Bob Mortimer at Manchester's infamous Ha&#xe7ienda nightclub.",
@@ -45,34 +62,62 @@ var factsList = [
     "Mark used to be terrified of Moths but now experiences only moderate anxiety unless the moth is particularly large.",
     "Mark enjoys the heady scent of a hot greenhouse full of tomato plants.",
     "Mark was once charged by an elephant. Luckily the elephant turned out to be bluffing.",
-    "Mark has a collection of over 150 board games.",
-    "Mark has lived in  windmill."
+    "Mark has a collection of over 150 board games. None of them are Monopoly.",
+    "Mark has lived in a windmill."
 ];
 
-var numFacts = factsList.length;
-var rand = Math.floor(Math.random()*numFacts);
+function writeRandomFax(){
+    // Select a random fact
+    var numFacts = factsList.length;
+    var rand = Math.floor(Math.random()*numFacts);
 
-asidePar.innerHTML = factsList[rand];
+    // Inject into the .aside element
+    asidePar.innerHTML = factsList[rand];
+}
+
+// Call RandomFax on pageload
+writeRandomFax();
+
+
+// Write a new random fact if the user clicks in the .aside element
+const aside = document.querySelector('.aside');
+
+aside.addEventListener('click',() =>{
+    writeRandomFax();
+})
 
 
 
+//////////////////////////////////////////////////////////////////
 // Add a Click eventListener to all of the project thumbnails
+
+// Create Nodelist of all the li tags within the ul.thumbnail element
 const thumbnails = document.querySelectorAll('ul.thumbnails li');
 
-thumbnails.forEach(thumbnail => {
+
+thumbnails.forEach(function(thumbnail,current) {
     thumbnail.addEventListener('click', () => {
-        //console.log(thumbnail);
+        
+        // When the thumbnail li is clicked...
+
+        // Hide all elements with a visible class
+        var visibles = document.getElementsByClassName("visible");
+        for(var i=0; i < visibles.length; i++){
+            visibles[i].classList.replace("visible","hidden");
+        }
+        
+        // Make the relevant info block visible
+        var clickedId = "info_" + (current + 1);
+        var clickedElement = document.getElementById(clickedId);
+        clickedElement.classList.replace("hidden","visible");
+        
+        
+        //console.log(thumbnail,clickedId);
+        
     })
 })
 
 
 
-
-
-// Call tickTock on page load, so time appears immediately
-tickTock();
-
-// Call tickTock every 1000ms, so time updates every second
-setInterval(tickTock, 1000)
 
 
